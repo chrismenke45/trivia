096645607c8026ea11300a678a1f860309744089
chrismenke45 <chrismenke45@gmail.com>
2023-03-22 16:28:29 -0700

    added getToken and #generateQueryParams to fetcher

--- src/functions/Fetcher.ts
+++ src/functions/Fetcher.ts
@@ -1,5 +1,11 @@
+interface QueryParamObjProp { 
+    [key: string]: string | number; 
+}
+
 class Fetcher {
     options: RequestInit;
+    baseURL: string;
+    token: string | null;
     constructor() {
         this.options = {
             method: "GET",
@@ -8,6 +14,7 @@ class Fetcher {
                 'Accept': 'application/json',
             }
         }
+        this.baseURL = "https://opentdb.com"
     }
     buildFormData(formInfoArray: string[][]): void {
         let data = new FormData()
@@ -15,6 +22,16 @@ class Fetcher {
             if (formInfo[1]) { data.append(formInfo[0], formInfo[1]) }
         })
         this.options.body = data
+    }
+    #generateQueryParams(values: QueryParamObjProp): string {
+        let queryStr = "?"
+        for (const [index, [key, value]] of Object.entries(Object.entries(values))) {
+            if (Number(index) !== 0) {
+                queryStr += "&"
+            }
+            queryStr += `${key}=${value}`
+        }
+        return queryStr
     }
     async fetchData(): Promise<any> {
         try {
@@ -31,6 +48,32 @@ class Fetcher {
         }
         
     }
+    async fetchToken(): Promise<any> {
+        try {
+            let url: string = `${this.baseURL}/api_token.php${this.#generateQueryParams({"command": "request"})}`
+            const res = await fetch(url, this.options)
+            if (!res.ok) {
+                console.error(`Error recieving token from ${this.baseURL}`)
+            }
+            const data = await res.json()
+            this.token =  data.token
+        }
+        catch (err) {
+            console.error(err)
+        }
+        
+    }
+}
+
+const generateQueryParams = (values: QueryParamObjProp): string => {
+    let queryStr = "?"
+    for (const [index, [key, value]] of Object.entries(Object.entries(values))) {
+        if (Number(index) !== 0) {
+            queryStr += "&"
+        }
+        queryStr += `${key}=${value}`
+    }
+    return queryStr
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import "./styles/index.css";
 import Fetcher from "./functions/Fetcher";
 const main = async () => {
     const fetcher = new Fetcher()
-    const data = await fetcher.fetchData()
-    console.log(data)
+    const data = await fetcher.fetchToken()
+    //console.log(data)
     console.log("yeehaw");
 
 };
