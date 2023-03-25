9f6cc9871c08df2123e47eecbc9f77effed953d8
chrismenke45 <chrismenke45@gmail.com>
2023-03-25 11:00:56 -0700

    made fetcheds for each necessary request

--- src/functions/Fetcher.ts
+++ src/functions/Fetcher.ts
@@ -16,26 +16,31 @@ class Fetcher {
         }
         this.baseURL = "https://opentdb.com"
     }
-    buildFormData(formInfoArray: string[][]): void {
-        let data = new FormData()
-        formInfoArray.forEach(formInfo => {
-            if (formInfo[1]) { data.append(formInfo[0], formInfo[1]) }
-        })
-        this.options.body = data
-    }
+    // buildFormData(formInfoArray: string[][]): void {
+    //     let data = new FormData()
+    //     formInfoArray.forEach(formInfo => {
+    //         if (formInfo[1]) { data.append(formInfo[0], formInfo[1]) }
+    //     })
+    //     this.options.body = data
+    // }
     #generateQueryParams(values: QueryParamObjProp): string {
-        let queryStr = "?"
-        for (const [index, [key, value]] of Object.entries(Object.entries(values))) {
-            if (Number(index) !== 0) {
-                queryStr += "&"
+        if (values) {
+            let queryStr = "?"
+            for (const [index, [key, value]] of Object.entries(Object.entries(values))) {
+                if (Number(index) !== 0) {
+                    queryStr += "&"
+                }
+                queryStr += `${key}=${value}`
             }
-            queryStr += `${key}=${value}`
+            return queryStr
+        } else {
+            return ""
         }
-        return queryStr
+        
     }
-    async fetchData(): Promise<any> {
+    async #basicFetch(path: string) {
+        let url: string = this.baseURL + path
         try {
-            let url: string = 'https://the-trivia-api.com/api/questions?limit=20&categories=science,history'
             const res = await fetch(url, this.options)
             if (!res.ok) {
                 throw new Error(`HTTP error! status: ${res.status}`);
@@ -46,34 +51,17 @@ class Fetcher {
         catch (err) {
             console.error(err)
         }
-        
+    }
+    async fetchQuestions(params: QueryParamObjProp): Promise<any> {
+        return await this.#basicFetch(`/api.php${this.#generateQueryParams(params)}`)
+    }
+    async fetchCategories(): Promise<any> {
+        return await this.#basicFetch(`/api_category.php`)
     }
     async fetchToken(): Promise<any> {
-        try {
-            let url: string = `${this.baseURL}/api_token.php${this.#generateQueryParams({"command": "request"})}`
-            const res = await fetch(url, this.options)
-            if (!res.ok) {
-                console.error(`Error recieving token from ${this.baseURL}`)
-            }
-            const data = await res.json()
-            this.token =  data.token
-        }
-        catch (err) {
-            console.error(err)
-        }
-        
+        const data = await this.#basicFetch(`/api_token.php${this.#generateQueryParams({"command": "request"})}`) 
+        this.token = data.token 
     }
-}
-
-const generateQueryParams = (values: QueryParamObjProp): string => {
-    let queryStr = "?"
-    for (const [index, [key, value]] of Object.entries(Object.entries(values))) {
-        if (Number(index) !== 0) {
-            queryStr += "&"
-        }
-        queryStr += `${key}=${value}`
-    }
-    return queryStr
 }
 
 
