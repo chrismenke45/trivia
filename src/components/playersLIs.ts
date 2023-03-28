a2575c952b7976cec710c1239c92a360af6e6d8a
chrismenke45 <chrismenke45@gmail.com>
2023-03-27 21:35:32 -0700

    added x to names list for removal (non functional)

--- src/components/playersLIs.ts
+++ src/components/playersLIs.ts
@@ -1,7 +1,17 @@
 const playersLIs = (playerArray: string[]):HTMLLIElement[] => {
     return playerArray.map(player => {
         let li: HTMLLIElement= document.createElement("li")
-        li.innerText = player
+        let span: HTMLSpanElement = document.createElement("span")
+        let div: HTMLDivElement = document.createElement("div")
+        span.innerText = player
+        div.appendChild(span)
+        div.insertAdjacentHTML('beforeend',`<i class="fa fa-times cursor-pointer"></i>`)
+        div.classList.add("w-full")
+        div.classList.add("flex")
+        div.classList.add("justify-between")
+        div.classList.add("items-center")
+        li.appendChild(div)
+        li.classList.add("w-full")
         return li
     })
 }
