6a2f87fce78dc6247ee35db3841fa393eeff2785
chrismenke45 <chrismenke45@gmail.com>
2023-04-01 14:00:34 -0700

    modularized game.ts

--- src/game.ts
+++ src/game.ts
@@ -5,26 +5,35 @@ import scoreDisplayLIs from "./components/scoreDisplayLIs";
 import { APIQuestion, QueryParamObjProp } from "./models"
 import askQuestionElement from "./components/askQuestionElement";
 
-const params = Object.fromEntries(  
+const params = Object.fromEntries(
     new URLSearchParams(window.location.search)
 )
-let players: Player[] = params["players"].split(",").map(name => new Player(name)) 
-console.log(players)
+let players: Player[] = params["players"].split(",").map(name => new Player(name))
 
 document.addEventListener("DOMContentLoaded", () => {
     const buildPage = async () => {
-        let fetcher = new Fetcher()
-        await fetcher.fetchToken()
-        let questionParams: QueryParamObjProp = {"amount": 20}
-        Number(params["category"]) && Object.assign(questionParams, {"category": params["category"]})
-        let data = await fetcher.fetchQuestions(questionParams)
-        const questions: APIQuestion[] = data.results
+        const questions: APIQuestion[] = await getQuestions(params, 0)
         console.log(questions)
-        let playerLIElements: HTMLLIElement[] = scoreDisplayLIs(players, 0)
-        playerLIElements.forEach(element => {
-            document.querySelector("#playerSummaries").appendChild(element)
-        })
+        updateScoreDisplay(players, 0)
         document.querySelector("main").appendChild(askQuestionElement(questions[0]))
     }
     buildPage()
 })
+
+const updateScoreDisplay = (players: Player[], currentPlayerIndex: number) => {
+    let playerSummariesList: HTMLUListElement = document.querySelector("#playerSummaries")
+    let playerLIElements: HTMLLIElement[] = scoreDisplayLIs(players, currentPlayerIndex)
+    playerSummariesList.innerHTML = ""
+    playerLIElements.forEach(element => {
+        playerSummariesList.appendChild(element)
+    })
+}
+
+const getQuestions = async (params: QueryParamObjProp, playerCount: number): Promise<APIQuestion[]> => {
+    let fetcher = new Fetcher()
+    await fetcher.fetchToken()
+    let questionParams: QueryParamObjProp = { "amount": 20 }
+    Number(params["category"]) && Object.assign(questionParams, { "category": params["category"] })
+    let data = await fetcher.fetchQuestions(questionParams)
+    return data.results
+}
