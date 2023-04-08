95f628a6679679408834c714eda4c43f7c9146b1
chrismenke45 <chrismenke45@gmail.com>
2023-04-08 10:58:45 -0700

    began question/answers layout

--- src/components/askQuestionElement.ts
+++ src/components/askQuestionElement.ts
@@ -5,13 +5,17 @@ const askQuestionElement = (question: APIQuestion):HTMLElement => {
     let article: HTMLElement = document.createElement("article")
     let p: HTMLParagraphElement = document.createElement("p")
     let ul: HTMLUListElement = document.createElement("ul")
+    article.classList.add("col-center")
+    ul.classList.add("grid", "grid-cols-2")
     question.question
     p.innerText = decodeURIComponent(question.question)
     article.appendChild(p)
     const answers: string[] = randomOrderAnswers(question)
     answers.forEach(answer => {
         let li: HTMLLIElement = document.createElement("li")
-        li.innerText = decodeURIComponent(answer)
+        let button: HTMLButtonElement = document.createElement("button")
+        button.innerText = decodeURIComponent(answer)
+        li.append(button)
         ul.append(li)
     })
     article.append(ul)
