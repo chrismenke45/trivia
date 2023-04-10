45b4ae226d44c55030a024c4783e1cfa14992135
chrismenke45 <chrismenke45@gmail.com>
2023-04-10 08:11:58 -0700

    fixed answer button sizing and changes grid to 1 column on small screen

--- src/components/askQuestionElement.ts
+++ src/components/askQuestionElement.ts
@@ -3,10 +3,11 @@ import { APIQuestion } from "../models"
 
 const askQuestionElement = (question: APIQuestion):HTMLElement => {
     let article: HTMLElement = document.createElement("article")
+    article.classList.add("m-4")
     let p: HTMLParagraphElement = document.createElement("p")
     let ul: HTMLUListElement = document.createElement("ul")
     article.classList.add("col-center")
-    ul.classList.add("grid", "grid-cols-2")
+    ul.classList.add("grid", "grid-cols-1", "gap-0.5", "sm:grid-cols-2")
     question.question
     p.innerText = decodeURIComponent(question.question)
     article.appendChild(p)
@@ -14,6 +15,7 @@ const askQuestionElement = (question: APIQuestion):HTMLElement => {
     answers.forEach(answer => {
         let li: HTMLLIElement = document.createElement("li")
         let button: HTMLButtonElement = document.createElement("button")
+        button.classList.add("w-full")
         button.innerText = decodeURIComponent(answer)
         li.append(button)
         ul.append(li)
