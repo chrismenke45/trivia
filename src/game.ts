359c95ce8ff785b09c1f7a5d17084005d4f997a0
chrismenke45 <chrismenke45@gmail.com>
2023-04-10 09:19:49 -0700

    game is playable and ends when questions run out, scores displayed

--- src/game.ts
+++ src/game.ts
@@ -9,20 +9,19 @@ const params = Object.fromEntries(
     new URLSearchParams(window.location.search)
 )
 let players: Player[] = params["players"].split(",").map(name => new Player(name))
+let currentQuestionIndex: number = 0
 
 document.addEventListener("DOMContentLoaded", () => {
     const buildPage = async () => {
         const questions: APIQuestion[] = await getQuestions(params, players.length)
         console.log(questions)
-        questions.forEach((question, index) => {
-            updateScoreDisplay(players, index)
-            document.querySelector("main").appendChild(askQuestionElement(question))
-        })
+        updateUI(players, currentQuestionIndex, questions)
     }
     buildPage()
 })
 
-const updateScoreDisplay = (players: Player[], currentPlayerIndex: number) => {
+const updateScoreDisplay = (players: Player[], currentPlayerIndex?: number) => {
+    console.log("update", currentPlayerIndex)
     let playerSummariesList: HTMLUListElement = document.querySelector("#playerSummaries")
     let playerLIElements: HTMLLIElement[] = scoreDisplayLIs(players, currentPlayerIndex)
     playerSummariesList.innerHTML = ""
@@ -38,4 +37,36 @@ const getQuestions = async (params: QueryParamObjProp, playerCount: number): Pro
     Number(params["category"]) && Object.assign(questionParams, { "category": params["category"] })
     let data = await fetcher.fetchQuestions(questionParams)
     return data.results
+}
+
+const updateUI = (players: Player[], currentIndex: number, questions: APIQuestion[]) => {
+    updateScoreDisplay(players, currentIndex % players.length)
+    document.querySelectorAll(".answer").forEach(answer => {
+        answer.removeEventListener("click", (e) => answerHandler(e, questions))
+    })
+    document.querySelector("main").innerHTML = ""
+    document.querySelector("main").appendChild(askQuestionElement(questions[currentIndex]))
+    document.querySelectorAll(".answer").forEach(answer => {
+        answer.addEventListener("click", (e) => answerHandler(e, questions))
+    })
+}
+
+const answerHandler = (e: Event, questions: APIQuestion[]) => {
+    const button = e.target as HTMLElement;
+    if (button.innerText === decodeURIComponent(questions[currentQuestionIndex].correct_answer)) {
+        console.log("correct")
+        players[currentQuestionIndex % players.length].addCount(1)
+    } else {
+        console.log("incorrect")
+        players[currentQuestionIndex % players.length].addCount(0)
+    }
+    console.log(button.innerText, currentQuestionIndex)
+    currentQuestionIndex++
+    if (currentQuestionIndex < questions.length) {
+        updateUI(players, currentQuestionIndex, questions)
+    } else {
+        updateScoreDisplay(players)
+        document.querySelector("main").innerHTML = "That's all the questions"
+    }
+    
 }
