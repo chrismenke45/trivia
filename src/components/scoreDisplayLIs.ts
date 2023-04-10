001283890c209062740affa0f61c8aeddb4a1b9a
chrismenke45 <chrismenke45@gmail.com>
2023-04-10 09:19:03 -0700

    score display reflects active player and is responsive

--- src/components/scoreDisplayLIs.ts
+++ src/components/scoreDisplayLIs.ts
@@ -1,13 +1,15 @@
 import Player from "../classes/Player"
 
-const scoreDisplayLIs = (playerArray: Player[], activeIndex: number):HTMLLIElement[] => {
+const scoreDisplayLIs = (playerArray: Player[], activeIndex?: number): HTMLLIElement[] => {
     return playerArray.map((player, index) => {
-        let li: HTMLLIElement= document.createElement("li")
+        let li: HTMLLIElement = document.createElement("li")
         let nameSpan: HTMLSpanElement = document.createElement("span")
         nameSpan.innerText = player.name
         let percentSpan: HTMLSpanElement = document.createElement("span")
         percentSpan.innerText = `${player.correctPercentage}%`
-        li.classList.add("flex", "justify-between", "mx-1", "p-1", "basis-24", "bg-red-100", "rounded", "border", "border-black")
+        li.classList.add("sm:flex", "justify-between", "mx-1", "p-1", "basis-24", "rounded", "border", "border-black")
+        li.classList.add(activeIndex !== undefined && activeIndex === index ? "bg-green-300" : "bg-red-100")
+        li.classList.add(activeIndex !== undefined && activeIndex === index ? "flex" : "hidden")
         li.appendChild(nameSpan)
         li.appendChild(percentSpan)
         return li
